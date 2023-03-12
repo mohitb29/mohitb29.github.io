e8475ca6e2e9022f6d68a0ca5275722a1a45a596
mohitb29 <mohitbairwa29@gmail.com>
2023-03-12 16:39:20 +0530

    updates 12/03/23

--- script.js
+++ script.js
@@ -15,39 +15,28 @@ function setHeaderClass(){
     window.addEventListener(evt, setHeaderClass);
   });
 
-
-//cursor js
-// const $bigBall = document.querySelector('.cursor__ball--big');
-// const $smallBall = document.querySelector('.cursor__ball--small');
-// const $hoverables = document.querySelectorAll('.hoverable');
-
-// Listeners
-// document.body.addEventListener('mousemove', onMouseMove);
-// for (let i = 0; i < $hoverables.length; i++) {if (window.CP.shouldStopExecution(0)) break;
-//   $hoverables[i].addEventListener('mouseenter', onMouseHover);
-//   $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
-// }
-
-// Move the cursor
-// window.CP.exitedLoop(0);function onMouseMove(e) {
-//   TweenMax.to($bigBall, .4, {
-//     x: e.pageX - 15,
-//     y: e.pageY - 15 });
-
-//   TweenMax.to($smallBall, .1, {
-//     x: e.pageX - 5,
-//     y: e.pageY - 7 });
-
-// }
-
-// // Hover an element
-// function onMouseHover() {
-//   TweenMax.to($bigBall, .3, {
-//     scale: 4 });
-
-// }
-// function onMouseHoverOut() {
-//   TweenMax.to($bigBall, .3, {
-//     scale: 1 });
-
-// }
+//slider
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 3,
+    direction: 'horizontal',
+    spaceBetween:16,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
